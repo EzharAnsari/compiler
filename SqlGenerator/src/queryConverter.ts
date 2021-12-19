7f27c938cdda55284c6c5a71955d9b90576c0908
EzharAnsari <ea199100@gmail.com>
2021-12-19 20:04:06 +0530

    Sql converter function added

--- SqlGenerator/src/queryConverter.ts
+++ SqlGenerator/src/queryConverter.ts
@@ -1,51 +1,72 @@
-queryGenerate() {
-  let i = 0;
-  let result = "SELECT ";
+import { IntermediateQuery, WhereClause, JoinClause, ObjectNode, ArrayNode, MemberNode, BinaryOpNode } from './intermediateQuery'
 
-  for(i=0; i<this.selectList.length; i++) {
-    result += this.selectList[i].value
-    if (this.selectList.length != (i+1))
+export function binaryOpNodeToString(value: BinaryOpNode): string {
+  let result = ''
+
+  if (value.left instanceof BinaryOpNode) {
+    result += '( '
+    result += binaryOpNodeToString(value.left)
+    result += ' )'
+  }
+  else if(value.left instanceof ObjectNode) {
+    result += value.left.tok.value
+  }
+
+  else if (value.left instanceof MemberNode) {
+    result += value.left.value.value
+      result += '.'
+      result += value.left.property.value
+  }
+
+  else if
+
+  return result
+}
+
+ export function sqlConverter(IQ: IntermediateQuery): string {
+  let i = 0;
+  let result = "SELECT "
+
+  for(i=0; i<IQ.selectList.length; i++) {
+    let select = IQ.selectList[i]
+
+    if (select instanceof ObjectNode) {
+      result += select.tok.value
+    }
+    else if(select instanceof MemberNode) {
+      result += select.value.value
+      result += '.'
+      result += select.property.value
+    }
+    
+    if (IQ.selectList.length != (i+1))
       result += ", ";
     else
       result += " " 
-  } 
-  
+  }
+
   result += "FROM ";
 
-  for(i=0; i<this.fromList.length; i++) {
-    result += this.fromList[i].value
-    if (this.fromList.length != (i+1))
+  for(i=0; i<IQ.fromList.length; i++) {
+    result += IQ.fromList[i].value
+    if (IQ.fromList.length != (i+1))
       result += ", ";
     else
       result += " " 
   }
 
-  result += "WHERE ";
-
-  for(i=0; i<this.conditionList.length; i++) {
-    let temConditionNode = this.conditionList[i];
-    result += temConditionNode.object.value;
-    result += temConditionNode.operator.type;
-    switch(temConditionNode.value.type) {
-      case INT:
-      case FLOAT:
-        result += temConditionNode.value.value;
-        break;
-      case STRING:
-        result += "'" + temConditionNode.value.value + "'";
-        break;
-      default:
-        console.log("Invalid constant type");
-    }
-    if (temConditionNode.logOpToNextCondition){
-      result += " " + temConditionNode.logOpToNextCondition.type;
-    }
-
-    if (this.conditionList.length != i+1) {
-      result += " ";
-    }
+  if(IQ.joinClause.isUsed) {
+    let joinClause = IQ.joinClause
+    result += joinClause.joinType
+    result += " JOIN "
+    result += joinClause.joinTable
+    result += " on "
+    result += binaryOpNodeToString(joinClause.joinCondition)
   }
-  result += ";"
-  return result;
 
+  if(IQ.whereClause.isUsed) {
+
+  }
+
+  return result
 }
